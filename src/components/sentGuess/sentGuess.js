53a7f97e2b5e4cc6cb0348ceb18dcc013ff1fada
Jose Mejia <josemejia91198@gmail.com>
2023-03-14 20:30:04 -0700

    forgot className whoops. now onto ex 3.

--- src/components/sentGuess/sentGuess.js
+++ src/components/sentGuess/sentGuess.js
@@ -4,7 +4,7 @@ function SentGuess({guessList,setGuessList}) {
   return (
   <div className="guess-results">
     {guessList.map((guess,index) => (
-      <p key={`${guess.guess}${index}`}>{guess.guess}</p>)
+      <p className="guess" key={`${guess.guess}${index}`}>{guess.guess}</p>)
       )
     }
   </div>);
