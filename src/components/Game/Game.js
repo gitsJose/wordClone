74ade59cb18fc2b94402a756a332ddf8bfc8c86d
Jose Mejia <josemejia91198@gmail.com>
2023-03-20 15:25:24 -0700

    Exercise 4 completed.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessForm  from '../guessForm'
+import GuessForm  from '../guessForm';
 import GuessResults from '../guessResults/guessResults';
+import { checkGuess } from '../../game-helpers';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
@@ -11,22 +13,28 @@ console.info({ answer });
 
 
 function Game() {
-  // const [guessList, setGuessList] = React.useState(['','','','',''])
   const [guessList, setGuessList] = React.useState([[''],[''],[''],[''],[''],['']])
   const [guessCount, setGuessCount] = React.useState(0)
   let frontEndCount = guessCount + 1;
 
   function handleSubmission(playerGuess, guessCount){
     let newList = [...guessList]
+    newList[guessCount] = checkGuess(playerGuess, answer)
+    // console.log("NewListTest",newList)
+
+    /* code works, trying something new - exercise 4
+    let newList = [...guessList]
     let splitGuess = playerGuess.split('')
     console.log(splitGuess, 'splitGuess')
     splitGuess.forEach((letter,index) => {
       newList[guessCount][index] = letter
     })
+    */
     setGuessList(newList)
     setGuessCount(guessCount + 1);
     console.log(guessCount,newList, newList[guessCount], 'Testing HandleSubmission / newList')
     console.log(guessCount,guessList, guessList[guessCount], 'Testing HandleSubmission / GuessList')
+
   }
 
   return <>
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {range} from '../../utils'
 
+
 function Grid({guessList, guessCount}) {
   let rows = range(0, 6, 1)
   let cols = range(0,5,1)
@@ -11,7 +12,7 @@ function Grid({guessList, guessCount}) {
       <p className="guess" key={`${index}`}>
         {cols.map((col,index) => {
           // return (<span className="cell" key={`${row}-${index}`}>{ !!guessCount ? guessList[guessCount-1][index] : ''}</span>)
-          return (<span className="cell" key={`${row}-${index}`}>{ guessList[row][index] ? guessList[row][index] : ''}</span>)
+          return (<span className={guessList[row][index] ? `cell ${guessList[row][index].status}` : 'cell'} key={`${row}-${index}`}>{ guessList[row][index] ? guessList[row][index].letter : ''}</span>)
         })}
       </p>
     )))}
